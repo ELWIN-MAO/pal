07b19512a9ccd0d98980b931354575781f3dde21
bjzhangqi <bjzhangqi@yeah.net>
2016-06-08 03:11:47 +0800

    add xaddtest.s

---
+++ chap05/xaddtest.s
@@ -0,0 +1,13 @@
+.section .data
+value:
+    .int 10
+.section .text
+.globl main
+main:
+    nop
+    movl $100, %eax
+    movl $200, %ebx
+    xadd %eax, %ebx
+    xadd %ebx, value
+    movl $1, %eax
+    int $0x80
